00d8241f5f550da994819cef9985821e9ac2b817
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-08-29 10:46:24 -0500

    refactored content markup and styles

--- src/components/Content.js
+++ src/components/Content.js
@@ -29,167 +29,149 @@ const StaticContent = () =>
       <div className="submissions">
         <ul className="list-group">
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={solidCircle} className="icon" cleanup={['height', 'width']} />
               Complete
-            </div>
-            <div className="label">Send Message To Requester</div>
-            <div className="summary">
+            </p>
+            <h1>Send Message To Requester</h1>
+            <p className="summary">
               At our current plan, luckyorange.com allows tracking for 3 sites.
               We would have to increase our plan to added
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Open
-            </div>
-            <div className="label">Work Order (9247AF)</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF)</h1>
+            <p className="summary">
               Content Change has been requested by James Davies
-            </div>
-            <div className="assignment">Matt B</div>
+            </p>
+            <p className="assignment">Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Pending (Approved)
-            </div>
-            <div className="label">Work Order (9247AF</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF</h1>
+            <p className="summary">
               General Marketing Request has been requested by John Sundberg
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={solidCircle} className="icon" cleanup={['height', 'width']} />
               Complete
-            </div>
-            <div className="label">Send Message To Requester</div>
-            <div className="summary">
+            </p>
+            <h1>Send Message To Requester</h1>
+            <p className="summary">
               At our current plan, luckyorange.com allows tracking for 3 sites.
               We would have to increase our plan to added
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Open
-            </div>
-            <div className="label">Work Order (9247AF)</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF)</h1>
+            <p className="summary">
               Content Change has been requested by James Davies
-            </div>
-            <div className="assignment">Matt B</div>
+            </p>
+            <p className="assignment">Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Pending (Approved)
-            </div>
-            <div className="label">Work Order (9247AF</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF</h1>
+            <p className="summary">
               General Marketing Request has been requested by John Sundberg
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={solidCircle} className="icon" cleanup={['height', 'width']} />
               Complete
-            </div>
-            <div className="label">Send Message To Requester</div>
-            <div className="summary">
+            </p>
+            <h1>Send Message To Requester</h1>
+            <p className="summary">
               At our current plan, luckyorange.com allows tracking for 3 sites.
               We would have to increase our plan to added
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Open
-            </div>
-            <div className="label">Work Order (9247AF)</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF)</h1>
+            <p className="summary">
               Content Change has been requested by James Davies
-            </div>
-            <div className="assignment">Matt B</div>
+            </p>
+            <p className="assignment">Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
           <li className="submission list-group-item">
-            <div className="status">
+            <p className="status">
               <SVGInline svg={emptyCircle} className="icon" cleanup={['height', 'width']} />
               Pending (Approved)
-            </div>
-            <div className="label">Work Order (9247AF</div>
-            <div className="summary">
+            </p>
+            <h1>Work Order (9247AF</h1>
+            <p className="summary">
               General Marketing Request has been requested by John Sundberg
-            </div>
-            <div className="assignment">Marketing &gt; Matt B</div>
+            </p>
+            <p className="assignment">Marketing &gt; Matt B</p>
             <ul className="timestamps list-group">
               <li className="list-group-item">Due in 6 hours</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Updated 7 days ago</li>
-              <li className="list-group-item separator">&bull;</li>
               <li className="list-group-item">Created 7 Days ago</li>
             </ul>
           </li>
