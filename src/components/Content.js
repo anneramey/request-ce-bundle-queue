dc7092bcbc4c648a8c15b24c7ed38506662bf33c
Matt Raykowski <matt.raykowski@kineticdata.com>
2017-09-28 15:18:56 -0500

    KIN-6: Basic notification 'toast' support.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import { NotificationsContainer } from './notifications/NotificationsContainer';
 import { FilterMenuContainer } from './FilterMenu/FilterMenuContainer';
 import { QueueItemContainer } from './QueueItem/QueueItem';
 import { QueueListContainer } from './queueList/QueueListContainer';
 
 export const Content = () =>
   <div className="content">
+    <NotificationsContainer />
     <Route path="/" exact render={() => <div>Please select a list</div>} />
     <Route path="/list/:filter" render={routeProps => <QueueListContainer {...routeProps} />} />
     <Route path="/item/:id" component={QueueItemContainer} />
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,9 +1,11 @@
 import { watchSubmissions } from './sagas/submissions';
 import { watchApp } from './sagas/app';
 import { watchQueue } from './sagas/queue';
+import { watchErrors } from './sagas/errors';
 
 export function* sagas() {
   yield [
+    watchErrors(),
     watchApp(),
     watchSubmissions(),
     watchQueue(),
