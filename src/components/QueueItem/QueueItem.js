d05b6bda85e6336f0a9905b813ed30d532f5ec24
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-10-13 13:49:15 -0500

    implements Grab It button on details page KIN-138

--- src/components/QueueItem/QueueItem.js
+++ src/components/QueueItem/QueueItem.js
@@ -17,6 +17,8 @@ export const QueueItem = ({
   workMenuOpen,
   openWorkMenu,
   closeWorkMenu,
+  assignedToMe,
+  grabIt,
 }) =>
   queueItem !== null && (
     <div className="queue-item-details two-panels">
@@ -68,9 +70,9 @@ export const QueueItem = ({
       <div className="right-panel">
         <button
           className="btn btn-primary work-grab-button"
-          onClick={openWorkMenu}
+          onClick={assignedToMe ? openWorkMenu : grabIt}
         >
-          Works / Grab It
+          {assignedToMe ? 'Work It' : 'Grab It'}
         </button>
       </div>
     </div>
@@ -81,10 +83,16 @@ export const mapStateToProps = (state, props) => ({
   currentFilterName: state.queue.currentFilter.name,
   id: props.match.params.id,
   workMenuOpen: state.queue.workMenuOpen,
+  profile: state.app.profile,
+  assignedToMe:
+    state.queue.currentItem &&
+    state.queue.currentItem.values['Assigned Individual'] ===
+      state.app.profile.username,
 });
 
 export const mapDispatchToProps = {
   fetchCurrentItem: actions.fetchCurrentItem,
+  updateCurrentItem: actions.updateCurrentItem,
   openWorkMenu: actions.openWorkMenu,
   closeWorkMenu: actions.closeWorkMenu,
 };
@@ -97,6 +105,11 @@ export const QueueItemContainer = compose(
       refreshItem();
       closeWorkMenu();
     },
+    grabIt: ({ updateCurrentItem, profile }) => () =>
+      updateCurrentItem({
+        'Assigned Individual': profile.username,
+        'Assigned Individual Display Name': profile.displayName,
+      }),
   }),
   lifecycle({
     componentWillMount() {
