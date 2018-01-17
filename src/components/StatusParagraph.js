aa7d5974f972f0a638387dcfa9b666be916f0c0c
Matt Raykowski <matt.raykowski@kineticdata.com>
2018-01-17 11:21:52 -0600

    KIN-333: Styled "pagination buttons" into status paragraph.

--- src/components/StatusParagraph.js
+++ src/components/StatusParagraph.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import SVGInline from 'react-svg-inline';
-import { UncontrolledTooltip } from 'reactstrap';
+import { UncontrolledTooltip, ButtonGroup, Button } from 'reactstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import solidCircle from 'font-awesome-svg-png/white/svg/circle.svg';
 import emptyCircle from 'font-awesome-svg-png/white/svg/circle-o.svg';
+import caretLeft from 'font-awesome-svg-png/white/svg/caret-left.svg';
+import caretRight from 'font-awesome-svg-png/white/svg/caret-right.svg';
 
 const CLOSED_STATUSES = ['Cancelled', 'Complete'];
 
@@ -24,28 +27,46 @@ const getStatusReason = queueItem => {
   }
 };
 
-export const StatusParagraph = ({ queueItem }) => (
-  <p className={getStatusClass(queueItem.values.Status)}>
-    <SVGInline
-      svg={
-        CLOSED_STATUSES.includes(queueItem.values.Status)
-          ? solidCircle
-          : emptyCircle
-      }
-      className="icon"
-    />
-    {queueItem.values.Status}
-    <span className="status-reason" id={getStatusId(queueItem)}>
-      {getStatusReason(queueItem)}
-    </span>
-    {queueItem.values.Status !== 'Open' && (
-      <UncontrolledTooltip
-        placement="top"
-        target={getStatusId(queueItem)}
-        delay={0}
-      >
+const PrevAndNextGroup = ({ prevAndNext }) => (
+  <ButtonGroup className="queue-details-nav btn-group-xs">
+    <LinkContainer to={`/item/${prevAndNext.prev}`}>
+      <Button color="secondary" outline disabled={!prevAndNext.prev}>
+        <SVGInline svg={caretLeft} className="icon" />
+      </Button>
+    </LinkContainer>
+    <LinkContainer to={`/item/${prevAndNext.next}`}>
+      <Button color="secondary" outline disabled={!prevAndNext.next}>
+        <SVGInline svg={caretRight} className="icon" />
+      </Button>
+    </LinkContainer>
+  </ButtonGroup>
+);
+
+export const StatusParagraph = ({ queueItem, prevAndNext }) => (
+  <div className="status-paragraph">
+    <p className={getStatusClass(queueItem.values.Status)}>
+      <SVGInline
+        svg={
+          CLOSED_STATUSES.includes(queueItem.values.Status)
+            ? solidCircle
+            : emptyCircle
+        }
+        className="icon"
+      />
+      {queueItem.values.Status}
+      <span className="status-reason" id={getStatusId(queueItem)}>
         {getStatusReason(queueItem)}
-      </UncontrolledTooltip>
-    )}
-  </p>
+      </span>
+      {queueItem.values.Status !== 'Open' && (
+        <UncontrolledTooltip
+          placement="top"
+          target={getStatusId(queueItem)}
+          delay={0}
+        >
+          {getStatusReason(queueItem)}
+        </UncontrolledTooltip>
+      )}
+    </p>
+    {prevAndNext && <PrevAndNextGroup prevAndNext={prevAndNext} />}
+  </div>
 );
