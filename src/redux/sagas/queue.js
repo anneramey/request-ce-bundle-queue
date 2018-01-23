7a09c13461aabf1d72ce50767807d9007582072b
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-01-23 14:56:05 -0600

    updated QueueList component to show error message if a status message for the filter exists KIN-133

--- src/redux/sagas/queue.js
+++ src/redux/sagas/queue.js
@@ -7,7 +7,8 @@ import { types, actions } from '../modules/queue';
 import { actions as errorActions } from '../modules/errors';
 
 export const ERROR_STATUS_STRING = 'There was a problem retrieving items.';
-export const TOO_MANY_STATUS_STRING = 'Your filter matches too many items.';
+export const TOO_MANY_STATUS_STRING =
+  'Your filter matches too many items. Try a more specific filter.';
 
 export const SUBMISSION_INCLUDES =
   'details,values,attributes,form,children,children.details,children.form,children.values,form.attributes,parent,parent.details,parent.values,parent.form,parent.form.kapp';
